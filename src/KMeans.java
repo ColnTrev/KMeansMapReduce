3a38cec2b39298eacb665b0161a8b6a4cc4b222d
Collin Gordon <colntrev@gmail.com>
2018-02-13 23:34:08 -0800

    Initial commit

---
+++ src/KMeans.java
@@ -0,0 +1,41 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by colntrev on 2/13/18.
+ */
+public class KMeans {
+    public static class KMeansMap extends Mapper<PointVector,PointVector,PointVector,PointVector>{
+        private final List<PointVector> centers = new ArrayList<>();
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+            Configuration conf = context.getConfiguration();
+            Path cents = new Path(conf.get("centroid.path"));
+            FileSystem fs = FileSystem.get(conf);
+            try(SequenceFile.Reader reader = new SequenceFile.Reader(fs,cents,conf)){
+                PointVector key = new PointVector();
+                while(reader.next(key)){
+                    PointVector clusterCenter = new PointVector(key);
+                    centers.add(clusterCenter);
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/PointVector.java
@@ -0,0 +1,46 @@
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by colntrev on 2/13/18.
+ */
+public class PointVector implements WritableComparable<PointVector> {
+    private double[] point;
+
+    public PointVector(){
+        super();
+    }
+
+    public PointVector(PointVector pv){
+        super();
+        int size = pv.point.length;
+        this.point = new double[size];
+        System.arraycopy(pv.point,0,this.point, 0, size);
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(point.length);
+        for(int i = 0; i < point.length; i++){
+            dataOutput.writeDouble(point[i]);
+        }
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        int size = dataInput.readInt();
+        point = new double[size];
+        for(int i = 0; i < point.length; i++){
+            point[i] = dataInput.readDouble();
+        }
+    }
+
+    @Override
+    public int compareTo(PointVector pv){
+
+        return 0;
+    }
+}
